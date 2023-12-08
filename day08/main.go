40648ea797c05de8faa816514773f6a8295f8a39
Andreas Kaempf <andreas.kaempf@soundience.com>
2023-12-08 18:13:02 +0100

    Day 8: some code simplification

--- day08/main.go
+++ day08/main.go
@@ -15,21 +15,21 @@ import (
 	"fmt"
 )
 
-// Node, with its left and right
+// Node, with next destinations for left and right branches
 type Node struct {
 	id, L, R string
 }
 
-// Map of nodes and path
+// Map of nodes and path (global)
 var nodes map[string]Node
 var path string
 
 func main() {
 
 	// Read the input file into path and dict of nodes
-	nodes = map[string]Node{} // initialize
+	nodes = map[string]Node{} // initialize map
 	fname := "sample3.txt"
-	fname = "input.txt"
+	fname = "input.txt" // uncomment to use main input file
 	for _, l := range readLines(fname) {
 		if len(path) == 0 {
 			path = l // First line is LRLR* path
@@ -40,21 +40,21 @@ func main() {
 	}
 
 	// Part 1: navigate path from AAA to ZZZ, count steps
-	fmt.Println("Part 1:", nSteps("AAA", "ZZZ", 0))
+	fmt.Println("Part 1:", nSteps("AAA", "ZZZ"))
 
 	// Part 2: first find all starting nodes, i.e., those that end in A
-	locs := []string{}
+	starts := []string{}
 	for k := range nodes {
 		if k[2] == 'A' {
-			locs = append(locs, k)
+			starts = append(starts, k)
 		}
 	}
 
 	// Get path lengths from each start to any dest that ends in Z; these
 	// seem to be repeating, so use them as the basis for finding an answer
 	lengths := []int{}
-	for _, s := range locs {
-		n := nSteps(s, "any", 0) // length of path to any "xxZ"
+	for _, s := range starts {
+		n := nSteps(s, "any") // length of path to any "xxZ"
 		lengths = append(lengths, n)
 	}
 
@@ -62,44 +62,36 @@ func main() {
 	// with all paths; multiplying them does not work, must be a common divisor
 	// or something, so this is a bit slow
 	fmt.Println("Lengths:", lengths)
-	lowest := lengths[0] // use the first length as repeating interval
-	steps := lowest
+	steps := lengths[0]
 	for {
-		// Check if the number of steps can be divided by all lengths
-		//fmt.Println("Trying", steps)
+		// Check if the number of steps can be divided by all lengths,
+		// stop if yes
 		foundIt := true
 		for i := 0; i < len(lengths); i++ {
 			if steps%lengths[i] != 0 {
 				foundIt = false
 			}
 		}
-
-		// If yes, then we have the answer
 		if foundIt {
 			break
 		}
 
 		// Otherwise, try the next increment
-		steps += lowest
+		steps += lengths[0]
 	}
 	fmt.Println("Part 2:", steps) // 22103062509257
 }
 
-// Find the number of steps to get from A to B, given starting
-// path position p. If B is "any", finds the first destination
-// that ends in 'Z'
-func nSteps(A, B string, p int) int {
-	var steps int
+// Find the number of steps to get from A to B, given starting path position.
+// If B is "any", finds the first destination that ends in 'Z'
+func nSteps(A, B string) int {
+	var steps, pos int
 	loc := A
-	pos := p
 	for loc != B { // continue until we reach destination
-		//fmt.Println(steps, pos, loc)
-		n, _ := nodes[loc] // get current node
-		//assert(ok, loc+" not found")
-		if path[pos] == 'L' { // Get next location
+		n, _ := nodes[loc]    // get current node
+		if path[pos] == 'L' { // get next location
 			loc = n.L
 		} else {
-			//assert(path[pos] == 'R', "Invalid path")
 			loc = n.R
 		}
 		steps++
@@ -107,12 +99,7 @@ func nSteps(A, B string, p int) int {
 		if pos >= len(path) {
 			pos = 0
 		}
-		if steps > 1000000 { // avoid endless loop, should never happen
-			fmt.Println("Aborting")
-			return -1
-		}
-		// If destination is "any", stop at the first destination that
-		// ends with 'Z'
+		// If dest "any", stop at first destination that ends with 'Z'
 		if B == "any" && loc[2] == 'Z' {
 			fmt.Println(A, "->", loc, "after", steps, "steps")
 			return steps // comment this to see the repeating intervals
