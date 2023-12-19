082e098360813355eb3c44bf88f895a0b6567faa
Andreas Kaempf <andreas.kaempf@soundience.com>
2023-12-19 06:06:33 +0100

    Solution for Day 18

--- day18/main.go
+++ day18/main.go
@@ -3,7 +3,8 @@
 // Given a set of instructions to draw a polygon consisting of just
 // horizontal and vertical lines, count the number of points that are
 // inside the polygon. For Part 2, instructions are revised to give a
-// much bigger shape, that cannot be computed in memory.
+// much bigger shape, that cannot be computed in memory. Did Part 1 using
+// simple recursive flood fill, Part 2 using go-geom library.
 //
 // AK, 18 Dec 2023
 
@@ -33,12 +34,117 @@ func main() {
 	lines = readLines(fname)
 
 	// Part 1: Add up the number of points coloured
-	//ans := part1()
-	fmt.Println("Part 1 (62, 95356):", area(false))   // 62, 95356
-	fmt.Println("Part 2 (952408144115):", area(true)) // 952408144115
+	fmt.Println("Part 1:", part1())
+
+	// Part 2: Same, but use geometry
+	fmt.Println("Part 2:", area(true))
 }
 
-// Get area of polygon outlined by instructions, using the Go-Geom library
+// Part 1: interpret the instructions as direction, distance, color,
+// draw & fill, return number of squares that are on
+func part1() int {
+
+	// Map the edge, starting from 0,0
+	p := Point{} // current location 0,0
+	points = map[Point]int{}
+	for _, l := range lines {
+
+		// Parse line
+		parts := strings.Split(l, " ")
+		dir := parts[0] // direction R/L/D/U
+		n := atoi(parts[1])
+		//color := parts[2]  // not used for Part 1
+
+		// Draw edges of shape
+		for i := 0; i < n; i++ {
+			if dir == "R" {
+				p.x++
+			} else if dir == "L" {
+				p.x--
+			} else if dir == "U" {
+				p.y--
+			} else {
+				p.y++
+			}
+			points[p] = 1
+		}
+	}
+
+	// Fill the shape, using the simple recursive algorithm, and
+	// return the number points filled including border
+	fill1(4, 2)
+	return len(points)
+}
+
+// Simple recursive algorithm to flood-fill the shape, starting with the
+// given position, which must be inside shape (otherwise program will crash,
+// so you know to try a different value).
+// Works for part 1, but too memory intensive for part 2
+func fill1(x, y int) {
+	points[Point{x, y}] = 1 // fill current point
+	if empty(x-1, y) {
+		fill1(x-1, y)
+	}
+	if empty(x+1, y) {
+		fill1(x+1, y)
+	}
+	if empty(x, y-1) {
+		fill1(x, y-1)
+	}
+	if empty(x, y+1) {
+		fill1(x, y+1)
+	}
+}
+
+// Part 2: interpret the instructions from just the color,
+// use different fill algorithm to avoid stack overflow
+// NOT USED
+func part2() int64 {
+
+	// Map the edge, starting from 0,0
+	fmt.Println("Creating map")
+	p := Point{} // current location 0,0
+	points = map[Point]int{}
+	for _, l := range lines {
+
+		// Parse direction and number from hex color, e.g., "(#1b58a2)"
+		// The first five hexadecimal digits encode the distance in meters
+		// as a five-digit hexadecimal number.
+		// The last hexadecimal digit encodes the direction to dig:
+		// 0 means R, 1 means D, 2 means L, and 3 means U.
+		parts := strings.Split(l, " ")
+		color := parts[2] // direction & number not used for Part 2
+		ns := color[2:7]
+		n, err := strconv.ParseInt(ns, 16, 64)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		dir := int(color[7] - '0')
+
+		// Draw edges of shape
+		for i := 0; i < int(n); i++ {
+			if dir == 0 { // right
+				p.x++
+			} else if dir == 2 { // left
+				p.x--
+			} else if dir == 3 { // up
+				p.y--
+			} else { // else 1 = down
+				p.y++
+			}
+			points[p] = 1 // use 1 to designate edge (fill is 2)
+		}
+	}
+
+	// Fill the shape
+	//fill2(4, 2)
+
+	// Return the number of points filled
+	return fill4() //len(points)
+}
+
+// Get area of polygon outlined by instructions, using the Go-Geom library,
+// used for Part 2
 func area(part2 bool) int64 {
 
 	// Collect the corner points that make up the polygon
@@ -94,93 +200,7 @@ func area(part2 bool) int64 {
 	return int64(a + boundary/2 - 2)
 }
 
-// Part 1: interpret the instructions as direction, distance, color,
-// draw & fill, return number of squares that are on
-func part1() int64 {
-
-	// Map the edge, starting from 0,0
-	p := Point{} // current location 0,0
-	points = map[Point]int{}
-	for _, l := range lines {
-
-		// Parse line
-		parts := strings.Split(l, " ")
-		dir := parts[0] // direction R/L/D/U
-		n := atoi(parts[1])
-		//color := parts[2]  // not used for Part 1
-
-		// Draw edges of shape
-		for i := 0; i < n; i++ {
-			if dir == "R" {
-				p.x++
-			} else if dir == "L" {
-				p.x--
-			} else if dir == "U" {
-				p.y--
-			} else {
-				p.y++
-			}
-			points[p] = 1
-		}
-	}
-
-	// Fill the shape, using the simple recursive algorithm
-	fill1(4, 2)
-	ans := len(points)
-	fmt.Println("Using recursive algorith:", ans)
-
-	//ans := fill4()
-	//draw()
-	//return ans // fill3()
-	return 0
-}
-
-// Part 2: interpret the instructions from just the color,
-// use different fill algorithm to avoid stack overflow
-func part2() int64 {
-
-	// Map the edge, starting from 0,0
-	fmt.Println("Creating map")
-	p := Point{} // current location 0,0
-	points = map[Point]int{}
-	for _, l := range lines {
-
-		// Parse direction and number from hex color, e.g., "(#1b58a2)"
-		// The first five hexadecimal digits encode the distance in meters
-		// as a five-digit hexadecimal number.
-		// The last hexadecimal digit encodes the direction to dig:
-		// 0 means R, 1 means D, 2 means L, and 3 means U.
-		parts := strings.Split(l, " ")
-		color := parts[2] // direction & number not used for Part 2
-		ns := color[2:7]
-		n, err := strconv.ParseInt(ns, 16, 64)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		dir := int(color[7] - '0')
-
-		// Draw edges of shape
-		for i := 0; i < int(n); i++ {
-			if dir == 0 { // right
-				p.x++
-			} else if dir == 2 { // left
-				p.x--
-			} else if dir == 3 { // up
-				p.y--
-			} else { // else 1 = down
-				p.y++
-			}
-			points[p] = 1 // use 1 to designate edge (fill is 2)
-		}
-	}
-
-	// Fill the shape
-	//fill2(4, 2)
-
-	// Return the number of points filled
-	return fill4() //len(points)
-}
-
+// NOT USED
 func fill4() int64 {
 
 	// Get edge points into a list
@@ -212,6 +232,7 @@ func fill4() int64 {
 	return 0
 }
 
+// NOT USED
 func fill3() int64 {
 
 	// Get min/max x and y
@@ -275,25 +296,5 @@ func draw() {
 			fmt.Print(ifElse(empty(x, y), ".", "#"))
 		}
 		fmt.Print("\n")
-	}
-}
-
-// Simple recursive algorithm to flood-fill the shape, starting with the
-// given position, which must be inside shape (otherwise program will crash,
-// so you know to try a different value).
-// Works for part 1, but too memory intensive for part 2
-func fill1(x, y int) {
-	points[Point{x, y}] = 1 // fill current point
-	if empty(x-1, y) {
-		fill1(x-1, y)
-	}
-	if empty(x+1, y) {
-		fill1(x+1, y)
-	}
-	if empty(x, y-1) {
-		fill1(x, y-1)
-	}
-	if empty(x, y+1) {
-		fill1(x, y+1)
 	}
 }
